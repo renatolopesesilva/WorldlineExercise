6582b219fb539d311e97faa86b7898c43a965843
Renato-RIT <renato@resolutionit.eu>
2025-09-09 23:19:38 +0200

    indentation

--- src/main/java/LargestNumber.java
+++ src/main/java/LargestNumber.java
@@ -5,13 +5,11 @@ public class LargestNumber {
 
     public static void main(String[] args) {
         LargestNumber l = new LargestNumber();
-        String test1 = l.largestNumber( new int[]{50,2,1,9});
-        String test2 = l.largestNumber( new int[]{5,50,56});
-        String test3 = l.largestNumber( new int[]{420,42,423});
-
-        assetEquals( "95021", test1 );
-        assetEquals( "56550", test2 );
-        assetEquals( "42423420", test3);
+        assetEquals("95021", l.largestNumber(new int[]{50, 2, 1, 9}));
+        assetEquals("56550", l.largestNumber(new int[]{5, 50, 56}));
+        assetEquals("42423420", l.largestNumber(new int[]{420, 42, 423}));
+        assetEquals("95021", l.largestNumber(new int[]{9, 2, 50, 1}));
+        assetEquals("95021", l.largestNumber(new int[]{1, 50, 2, 9}));
     }
 
     public String largestNumber(int[] nums) {
@@ -19,7 +17,7 @@ public class LargestNumber {
         for (int num : nums) {
             s.add(String.valueOf(num));
         }
-        s.sort( (a,b) -> (b+a).compareTo(a+b));
+        s.sort((a, b) -> (b + a).compareTo(a + b));
         return String.join("", s);
     }
 
@@ -28,7 +26,7 @@ public class LargestNumber {
         if (!expected.equals(actual)) {
             throw new AssertionError(String.format("Expected %s but got %s", expected, actual));
         } else {
-            System.out.println("Passed");
+            System.out.printf("Passed. %s%n", actual);
         }
     }
 }
